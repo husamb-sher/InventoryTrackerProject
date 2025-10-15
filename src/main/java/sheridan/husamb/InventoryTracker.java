2d206a103ded30c1b70c97eb30abcad6b840c465
Bassam Husam <husamb@sheridancollege.ca>
2025-10-14 20:43:47 -0400

    added low stock alerts by bassam husam

--- src/main/java/sheridan/husamb/InventoryTracker.java
+++ src/main/java/sheridan/husamb/InventoryTracker.java
@@ -5,7 +5,9 @@ public class InventoryTracker {
     public static void main(String[] args) {
         System.out.println("Inventory Tracker initialized.");
     }
-
+    public void alertLowStock() {
+        System.out.println("Alert: Low stock detected!");
+    }
     public void checkStockLevel() {
         System.out.println("Checking stock levels...");
     }
