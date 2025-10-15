5345a7deeef693806fe167d860137992d09916a8
Bassam Husam <husamb@sheridancollege.ca>
2025-10-14 20:42:39 -0400

    implemented stock checking by bassam husam

--- src/main/java/sheridan/husamb/InventoryTracker.java
+++ src/main/java/sheridan/husamb/InventoryTracker.java
@@ -1,7 +1,12 @@
 package sheridan.husamb;
 
 public class InventoryTracker {
-        public static void main(String[] args) {
-        System.out.println("Inventory Tracker Online.");
+
+    public static void main(String[] args) {
+        System.out.println("Inventory Tracker initialized.");
+    }
+
+    public void checkStockLevel() {
+        System.out.println("Checking stock levels...");
     }
 }
